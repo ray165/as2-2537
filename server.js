f410b9ff78441699ec84a1d3a43616893054b1fd
Mazianos <mazin.marwan@gmail.com>
2021-05-06 18:39:35 -0700

    Fiddled with server.js and a DELETE request. Also added deleteRow.js with the DELETE ajax call.

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const PORT = 8000;
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const { MongoClient } = require("mongodb");
+const {
+  MongoClient, ObjectID
+} = require("mongodb");
 // const http = require("http");
 
 app.use("/js", express.static("js"));
@@ -13,95 +15,16 @@ app.use("/css", express.static("css"));
 const credentials = fs.readFileSync("./cert.pem");
 
 const client = new MongoClient(
-  "mongodb+srv://wecycle-vancouver.2hson.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority",
-  {
+  "mongodb+srv://wecycle-vancouver.2hson.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority", {
     sslKey: credentials,
     sslCert: credentials,
+    useUnifiedTopology: true
   }
 );
 
-// const database = client.db("WecycleMain");
-// const collection = database.collection("Users");
-
-async function run() {
-  try {
-    await client.connect();
-    // -->
-    const database = client.db("WecycleMain");
-    const collection = database.collection("Users");
-    const docCount = await collection.countDocuments({});
-    console.log(docCount, "running");
-    let testGet = await collection.find({}).toArray();
-    console.log(testGet);
-
-    // collection.insertOne({
-    //   name : "Johnson Lau",
-    //   contactNumber : "236-777-5240",
-    //   bottlesDonated : 50,
-    //   bottlesTaken : 4,
-    //   address : "700 Ioco Road, Port Moody BC"
-    // });
-
-    // collection.insertMany([{
-
-    // }])
-
-    // removeFromDB({ name: "" });
-    // addToUsersCollection(
-    //   "Raymond Wong",
-    //   "778-876-5432",
-    //   0,
-    //   120,
-    //   "Heritage Mountain"
-    // );
-    // updateCollectionOnID({name : "Raymond Wong"}, {address : "123 Las Vegas Rd, LA"});
-    console.log(testGet);
-
-    // perform actions using client
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-function removeFromDB(objectToRemove) {
-  client.connect();
-  client.db("WecycleMain").collection("Users").deleteOne(objectToRemove);
-  console.log("deleted successfully!");
-  client.close();
-}
-
-function addToUsersCollection(
-  nameValue,
-  contactNumberValue,
-  bottlesDonatedValue,
-  bottlesTakenValue,
-  addressValue
-) {
-  client.connect();
-  client.db("WecycleMain").collection("Users").insertOne({
-    name: nameValue,
-    contactNumber: contactNumberValue,
-    bottlesDonated: bottlesDonatedValue,
-    bottlesTaken: bottlesTakenValue,
-    address: addressValue,
-  });
-}
-
-function updateCollectionOnID(searchValue, key, newValue) {
-  client.connect();
-  client.db("WecycleMain").collection("Users").updateOne(
-    {
-      _id: searchValue,
-    },
-    {
-      $set: { key: newValue },
-    }
-  );
-}
+client.connect();
 
 console.log("check before the db run");
-run().catch(console.dir);
 
 // EXPRESS METHODS
 
@@ -110,7 +33,11 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create", function (req, res) {
-  res.send({ status: "success", rows, results });
+  res.send({
+    status: "success",
+    rows,
+    results
+  });
 });
 
 app.get("/read-table", function (req, res) {
@@ -121,28 +48,35 @@ app.get("/read-table", function (req, res) {
   async function grabData() {
     await client.connect();
     client.db("WecycleMain").collection("Users")
-    .find()
-    .toArray()
-    .then((data) => {
-      console.log(data);
-      res.json(data); //is this part wrong?
-    })
-    .catch((error) => console.error(error));
+      .find()
+      .toArray()
+      .then((data) => {
+        console.log(data);
+        res.json(data); //is this part wrong?
+      })
+      .catch((error) => console.error(error));
   };
 
   grabData();
 
-    // client.close(); // closing the client will break the program. Why?
+  // client.close(); // closing the client will break the program. Why?
 
 
 });
 
 app.post("/update-table", function (req, res) {
-  res.send({ status: "success", rows, results });
+  res.send({
+    status: "success",
+    rows,
+    results
+  });
 });
 
-app.post("/delete-row", function (req, res) {
-  res.send({ status: "success", rows, results });
+app.delete("/delete-row", function (req, res) {
+  let rowId = req.params.id;
+  console.log(id);
+  client.db("WecycleMain").collection("Users").findOneAndDelete({ _id: rowId});
+  
 });
 
 app.listen(PORT, () => {
