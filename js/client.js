7098f27d2fb96a8f21791d56ae31a76a8d238479
Yomamajohn <yichenglau@gmail.com>
2021-05-06 21:18:54 -0700

    Commented stuff out

--- js/client.js
+++ js/client.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-  console.log("hello world!");
+      console.log("hello world!");
 
-  function readTable() {
-    $.ajax({
-      url: "/read-table",
-      dataType: "json",
-      type: "GET",
-      success: function (data) {
-        console.log("SUCCESS JSON ARRAY:", data);
-        // for loop to build the table
-        let str = `<table class="table table-striped" id="dataTable"><tr>
+      function readTable() {
+        $.ajax({
+          url: "/read-table",
+          dataType: "json",
+          type: "GET",
+          success: function (data) {
+            console.log("SUCCESS JSON ARRAY:", data);
+            // for loop to build the table
+            let str = `<table class="table table-striped" id="dataTable"><tr>
               <th class="id_header"><span>ID</span></th>
               <th class="name_header"><span>Name</span></th>
               <th class="contactNumber_header"><span>Contact Number</span></th>
@@ -18,95 +18,121 @@ $(document).ready(function () {
               <th class="address_header"><span>Address</span></th>
               <th class="deleteRow"><span>Delete Row</span></th>
             </tr>`;
-        // console.log(data[0]);
-        for (let i = 0; i < data.length; i++) {
-          let row = data[i];
-          console.log("row", row);
-          str +=
-            "<tr><td class='id'>" +
-            row._id +
-            "</td><td id ='name'>" +
-            row.name +
-            "</td><td id='contactNumber'><span>" +
-            row.contactNumber +
-            "</td><td id='bottlesDonated'><span>" +
-            row.bottlesDonated +
-            "</td><td id='bottlesTaken'><span>" +
-            row.bottlesTaken +
-            "</td><td id='address'><span>" +
-            row.address +
-            "</span></td><td class='deleteBtn'>" +
-            `<button type="button" class="delete-button" id="${row._id}">Delete Row</button></td>`;
-        }
-        str += "</tr></table>";
-        $("#root").html(str);
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        $("#root").text(jqXHR.statusText);
-        console.log("ERROR:", jqXHR, textStatus, errorThrown);
-      },
-    });
-  };
-  readTable();
+            // console.log(data[0]);
+            for (let i = 0; i < data.length; i++) {
+              let row = data[i];
+              console.log("row", row);
+              str +=
+                "<tr><td class='id'>" +
+                row._id +
+                "</td><td class ='name'>" +
+                row.name +
+                "</td><td class='contactNumber'><span>" +
+                row.contactNumber +
+                "</td><td class='bottlesDonated'><span>" +
+                row.bottlesDonated +
+                "</td><td class='bottlesTaken'><span>" +
+                row.bottlesTaken +
+                "</td><td class='address'><span>" +
+                row.address +
+                "</span></td><td class='deleteBtn'>" +
+                `<button type="button" class="delete-button" id="${row._id}">Delete Row</button></td>`;
+            }
+            str += "</tr></table>";
+            $("#root").html(str);
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            $("#root").text(jqXHR.statusText);
+            console.log("ERROR:", jqXHR, textStatus, errorThrown);
+          },
+        });
+      };
+      readTable();
 
-  // *******************************
-  // UPDATE SECITON INDIVIDUAL CELLS
-  // *******************************
-  $(document).on("click", "span", function () {
-    console.log("table clicked!");
-  });
+      // *******************************
+      // UPDATE SECITON INDIVIDUAL CELLS
+      // *******************************
+      // $(document).on("click", "span", function () {
+      //       if ($(this).parent().attr('class') === 'email') {
 
-  $("#createTable").click(function (e) {
-    e.preventDefault();
+      //         let spanText = $(this).text();
+      //         let td = $(this).parent();
+      //         let input = $("<input type='text' value='" + spanText + "'>");
 
-    $("#name").val("");
-    $("#contactNumber").val("");
-    $("#bottleDonated").val("");
-    $("#bottlesTaken").val("");
-    $("#address").val("");
+      //         td.html(input);
 
-    $.ajax({
-      url: "/create-table",
-      dataType: "json",
-      type: "POST",
-      data: formData,
-      success: function (data) {
-        $("#status").html("DB updated.");
-        //getTable
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        $("#p2").text(jqXHR.statusText);
-        console.log("ERROR:", jqXHR, textStatus, errorThrown);
-      },
-    });
-  });
-  $("#root").on(function (e) {
-    // don't allow the anchor to visit the link
-    e.preventDefault();
-  });
-});
+      //         $(input).keyup(function (e) {
+      //           let val = null;
+      //           let span = null;
+      //           if (e.which == 13) {
 
-$(".delete-button").click(function (e) {
-  e.preventDefault();
-  console.log("Pressed delete.");
+      //             val = $(input).val();
+      //             span = $("<span>" + val + "</span>");
+      //             td.html(span);
 
-  let rowId = $(this).find("id").txt();
-  console.log(rowId);
+      //             console.log(td.parent().find("[class='id']")[0]);
 
-  // $.ajax({
-  //   url: "/delete-row/" + rowId,
-  //   method: 'DELETE',
-  //   contentType: "application/json",
-  //   type: "POST",
-  //   success: function (response) {
-  //     console.log("Deleted? Maybe");
-  //     client.db("WecycleMain").collection("Users").findOneAndDelete({
-  //       _id: rowId
-  //     });
-  //   },
-  //   error: function (jqXHR, textStatus, errorThrown) {
-  //     $("#root").text(jqXHR.statusText);
-  //     console.log("ERROR:", jqXHR, textStatus, errorThrown);
-  //   }
-  // });
-});
+      //             let dataToSend = {
+      //               id: td.parent().find("[class='id']").html(),
+      //               email: val
+      //             };
+      //           }
+      //         }); 
+      //         console.log("table clicked!");
+      //         }});
+
+
+              $("#createTable").click(function (e) {
+                e.preventDefault();
+
+                $("#name").val("");
+                $("#contactNumber").val("");
+                $("#bottleDonated").val("");
+                $("#bottlesTaken").val("");
+                $("#address").val("");
+
+                $.ajax({
+                  url: "/create-table",
+                  dataType: "json",
+                  type: "POST",
+                  data: formData,
+                  success: function (data) {
+                    $("#status").html("DB updated.");
+                    //getTable
+                  },
+                  error: function (jqXHR, textStatus, errorThrown) {
+                    $("#p2").text(jqXHR.statusText);
+                    console.log("ERROR:", jqXHR, textStatus, errorThrown);
+                  },
+                });
+              });
+              $("#root").on(function (e) {
+                // don't allow the anchor to visit the link
+                e.preventDefault();
+              });
+            });
+
+          $(".delete-button").click(function (e) {
+            e.preventDefault();
+            console.log("Pressed delete.");
+
+            let rowId = $(this).find("id").txt();
+            console.log(rowId);
+
+            // $.ajax({
+            //   url: "/delete-row/" + rowId,
+            //   method: 'DELETE',
+            //   contentType: "application/json",
+            //   type: "POST",
+            //   success: function (response) {
+            //     console.log("Deleted? Maybe");
+            //     client.db("WecycleMain").collection("Users").findOneAndDelete({
+            //       _id: rowId
+            //     });
+            //   },
+            //   error: function (jqXHR, textStatus, errorThrown) {
+            //     $("#root").text(jqXHR.statusText);
+            //     console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            //   }
+            // });
+          });
